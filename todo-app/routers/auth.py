f4c334081b012b73b1f5aa78feb027b7c9f5b64b
Hoon <53715807+totohoon02@users.noreply.github.com>
2024-09-08 21:25:16 +0900

    todoapp basic auth

--- todo-app/routers/auth.py
+++ todo-app/routers/auth.py
@@ -5,6 +5,7 @@ from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from database import session
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 # router setting
 router = APIRouter(prefix="/auth", tags=['auth'])
@@ -24,6 +25,18 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+
+    if not user:
+        return False
+
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+
+    return True
+
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
                       create_user_request: CreateUserRequest):
@@ -38,3 +51,13 @@ async def create_user(db: db_dependency,
     )
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Faild Authentication'
+    return 'Success Authentication'
